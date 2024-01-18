1cfea32ad31a9132ebe0abf34e9bbbe5db53de80
pgdev1729 <pranav.gundu@protonmail.com>
2024-01-17 20:15:55 -0500

    meep meep #2

--- MeepMeep/src/main/java/com/example/meepmeep/tests/TwoBotTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/tests/TwoBotTesting.java
@@ -10,7 +10,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class TwoBotTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(400);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         // Declare our first bot
         RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
@@ -19,41 +19,16 @@ public class TwoBotTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(0, 0, 180))
                 .build());
 
-        // Declare out second bot
-        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be red
-                .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
 
-        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(new Pose2d(30, 30, Math.toRadians(180)))
-                .strafeTo(new Vector2d(60, 45))
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 // Add both of our declared bot entities
                 .addEntity(myFirstBot)
-                .addEntity(mySecondBot)
                 .start();
     }
 }
